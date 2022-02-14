0f5b294fc2671bae9ef731d810b63b3fe60f21b0
hyeonjiChu <guswl8280@naver.com>
2022-02-14 09:25:48 +0900

    singleton

---
+++ AndroidSamples/sample35 singleton/app/src/main/java/com/example/sample35/SubActivity.kt
@@ -0,0 +1,15 @@
+package com.example.sample35
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class SubActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sub)
+
+        val tv = findViewById<TextView>(R.id.textView)
+        tv.text = Singleton.chicken
+    }
+}
